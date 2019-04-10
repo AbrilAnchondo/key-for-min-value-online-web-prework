2d3e449ce41657cd181c7e14b88b3b8b7ec3625a
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-10 01:21:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash = {}
-    nil 
-  end
-  new_array = []
-  name_hash.collect do |key, value|
-    new_array = new_array.push(value)
-    
+ 
 end
