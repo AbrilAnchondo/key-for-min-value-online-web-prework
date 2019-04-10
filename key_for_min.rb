8282540cc8079651e55edb55b9a1f8a5ba667e99
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-10 02:07:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 0 
+  min_value = Float::INFINITY 
   key_for_min_value= nil 
   name_hash.each_pair do |key, value|
     if value
